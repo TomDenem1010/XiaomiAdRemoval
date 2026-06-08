e7d9518a1d09dec3d8fc7a00b5e783b5a059d53a
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2026-06-08 17:24:59 +0200

    NEW: clearAds.bat

---
+++ clearAds.bat
@@ -0,0 +1,46 @@
+@echo off
+setlocal
+
+set ADB=%~dp0platform-tools\adb.exe
+
+if not exist "%ADB%" (
+    echo HIBA: Nem talalhato az adb.exe
+    echo Elvart hely: %ADB%
+    pause
+    exit /b 1
+)
+
+echo.
+echo ===== Xiaomi reklam es bloat eltavolito =====
+echo.
+
+"%ADB%" devices
+
+echo.
+echo ===== Eltavolitas indul =====
+echo.
+
+call :REMOVE com.miui.analytics "Analytics"
+call :REMOVE com.miui.android.fashiongallery "Wallpaper Carousel"
+call :REMOVE com.miui.bugreport "Hibajelentesek"
+call :REMOVE com.miui.daemon "Hatterszolgaltatas, telemetria jellegu funkciok"
+call :REMOVE com.miui.msa.global "MSA reklam szolgaltatas"
+call :REMOVE com.miui.yellowpage "Hivoazonosito, uzleti ajanlasok"
+
+call :REMOVE com.xiaomi.discover "Discover"
+call :REMOVE com.xiaomi.mipicks "GetApps"
+call :REMOVE com.xiaomi.ugd "Xiaomi User Growth"
+
+echo ----------------------------------------
+echo.
+echo ===== Kesz =====
+echo.
+pause
+exit /b
+
+:REMOVE
+echo ----------------------------------------
+echo %~2
+echo Package: %~1
+"%ADB%" shell pm uninstall --user 0 %~1
+exit /b
